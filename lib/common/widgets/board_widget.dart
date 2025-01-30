3ac7f6495adb87c177fbe50e46ebacd1f9646825
anujjainwork <jainanuj.work@gmail.com>
2025-01-30 19:33:05 +0530

    decoupled the 1v1 logic with boardgamelogic

--- lib/common/widgets/board_widget.dart
+++ lib/common/widgets/board_widget.dart
@@ -1,3 +1,4 @@
+import 'package:chess/features/1v1_mode/cubit/one_vs_one_cubit.dart';
 import 'package:chess/features/board/business/enums/player_type_enum.dart';
 import 'package:chess/features/board/data/model/cell_model.dart';
 import 'package:chess/features/board/logic/bloc/board_logic_bloc.dart';
@@ -8,17 +9,20 @@ import 'package:chess/features/board/logic/cubit/move_history_cubit.dart';
 import 'package:flutter/material.dart';
 
 Widget getBoardGameWidget(
-    List<BoardCellModel> initialBoard, BoardLogicBloc bloc, BoardLogicState state, MoveHistoryState moveHistoryState,BuildContext context) {
+    List<BoardCellModel> initialBoard,
+    BoardLogicBloc bloc,
+    BoardLogicState state,
+    MoveHistoryState moveHistoryState,
+    OneVsOneCubit oneVsOneCubit,
+    BuildContext context) {
   List<BoardCellModel> board = initialBoard;
   int? selectedCellIndex;
   List<int>? validMoves;
   List<int>? attackingPiecesIndices;
 
-  if(moveHistoryState is MoveRedo || moveHistoryState is MoveUndo)
-  {
+  if (moveHistoryState is MoveRedo || moveHistoryState is MoveUndo) {
     initialBoard = moveHistoryState.props[0] as List<BoardCellModel>;
-  }
-  else if (state is BoardLoaded) {
+  } else if (state is BoardLoaded) {
     board = state.board;
   } else if (state is ValidMovesHighlighted) {
     board = state.board;
@@ -35,19 +39,20 @@ Widget getBoardGameWidget(
   final rowLabels = ['8', '7', '6', '5', '4', '3', '2', '1'];
 
   // Dynamically calculate board and cell sizes
-  final double boardSize = getDynamicWidth(context, 85); // Use 85% of the screen width for the board
+  final double boardSize =
+      getDynamicWidth(context, 85); // Use 85% of the screen width for the board
   final double cellSize = boardSize / 8;
-  final double labelSize = getDynamicHeight(context, 4); // Adjust percentage for label size
+  final double labelSize =
+      getDynamicHeight(context, 4); // Adjust percentage for label size
 
   return Center(
     child: Container(
       padding: EdgeInsets.symmetric(vertical: getDynamicHeight(context, 1.5)),
       width: getDynamicWidth(context, 97), // Outer container width
       decoration: BoxDecoration(
-        color: AppColors.boxColor1,
-        borderRadius: BorderRadius.circular(12.0),
-        gradient: AppColors.boardWidgetGradient
-      ),
+          color: AppColors.boxColor1,
+          borderRadius: BorderRadius.circular(12.0),
+          gradient: AppColors.boardWidgetGradient),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -63,7 +68,8 @@ Widget getBoardGameWidget(
                     child: Center(
                       child: Text(
                         rowLabels[index],
-                        style: TextStyle(fontSize: labelSize * 0.4, color: Colors.white),
+                        style: TextStyle(
+                            fontSize: labelSize * 0.4, color: Colors.white),
                       ),
                     ),
                   );
@@ -88,9 +94,11 @@ Widget getBoardGameWidget(
                     return GestureDetector(
                       onTap: () {
                         if (selectedCellIndex == null) {
-                          bloc.add(SelectPiece(index, attackingPiecesIndices));
+                          oneVsOneCubit.selectPieceEvent(index, attackingPiecesIndices);
+                          // bloc.add(SelectPiece(index, attackingPiecesIndices));
                         } else if (selectedCellIndex == index) {
-                          bloc.add(DeselectPiece());
+                          oneVsOneCubit.deselectPieceEvent();
+                          // bloc.add(DeselectPiece());
                         } else {
                           bloc.add(MovePiece(selectedCellIndex, index));
                         }
@@ -108,10 +116,16 @@ Widget getBoardGameWidget(
                                       : AppColors.blackCellColor),
                         ),
                         child: board[index].hasPiece
-                            ? Transform.rotate(angle: board[index].pieceEntity!.playerType==PlayerType.white?0:3.14159,
-                            child: Center(
-                                child: getPieceIcon(board[index].pieceEntity!.pieceId),
-                              ),)
+                            ? Transform.rotate(
+                                angle: board[index].pieceEntity!.playerType ==
+                                        PlayerType.white
+                                    ? 0
+                                    : 3.14159,
+                                child: Center(
+                                  child: getPieceIcon(
+                                      board[index].pieceEntity!.pieceId),
+                                ),
+                              )
                             : null,
                       ),
                     );
@@ -121,21 +135,26 @@ Widget getBoardGameWidget(
             ],
           ),
           // Column labels (A to H)
-          SizedBox(height: getDynamicHeight(context, 1.5)), // Padding between board and column labels
+          SizedBox(
+              height: getDynamicHeight(
+                  context, 1.5)), // Padding between board and column labels
           Row(
             children: [
-              SizedBox(width: labelSize), // Fixed width to align with row labels
+              SizedBox(
+                  width: labelSize), // Fixed width to align with row labels
               SizedBox(
                 width: boardSize,
                 child: Row(
                   children: List.generate(8, (index) {
                     return SizedBox(
                       width: cellSize, // Match cell size
-                      height: labelSize * 0.8, // Adjust height for column labels
+                      height:
+                          labelSize * 0.8, // Adjust height for column labels
                       child: Center(
                         child: Text(
                           labels[index],
-                          style: TextStyle(fontSize: labelSize * 0.4, color: Colors.white),
+                          style: TextStyle(
+                              fontSize: labelSize * 0.4, color: Colors.white),
                         ),
                       ),
                     );
--- lib/common/widgets/confirmation_draw_resign_widget.dart
+++ lib/common/widgets/confirmation_draw_resign_widget.dart
@@ -1,4 +1,5 @@
 import 'package:chess/common/utils.dart';
+import 'package:chess/features/1v1_mode/cubit/one_vs_one_cubit.dart';
 import 'package:chess/features/board/business/enums/player_type_enum.dart';
 import 'package:chess/features/board/logic/bloc/board_bloc_builder.dart';
 import 'package:chess/features/board/logic/bloc/game_status_bloc.dart';
@@ -6,13 +7,13 @@ import 'package:chess/features/board/logic/cubit/move_history_cubit.dart';
 import 'package:flutter/material.dart';
 
 Widget getGameDrawOrResignWidget(BuildContext context, GameStatusBloc gameStatusBloc,
-    GameStatusState gameStatusState, MoveHistoryCubit moveHistoryCubit, bool isDrawCalled) {
+    GameStatusState gameStatusState, MoveHistoryCubit moveHistoryCubit, bool isDrawCalled, OneVsOneCubit oneVsOneCubit) {
   return SizedBox(
     height: getDynamicHeight(context, 90),
     width: getDynamicWidth(context, 100),
     child: Stack(
       children: [
-        boardGameBlocBuilder(gameStatusBloc,moveHistoryCubit),
+        boardGameBlocBuilder(gameStatusBloc,moveHistoryCubit,oneVsOneCubit),
         Align(
             alignment: isDrawCalled
                 ? gameStatusState.player == PlayerType.white
